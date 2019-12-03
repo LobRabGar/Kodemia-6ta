9e02eab665447047b16160162c29cbe4f3eff8cf
lobsang <lob_slash@live.com>
2019-12-03 17:13:19 -0600

    ejercicio de tarea

--- js/main.js
+++ js/main.js
@@ -7,3 +7,16 @@ var Cuc = 26.5 / 0.0511285
 console.log( "tengo " + PesoMex / USDolar + "USD")
 console.log("tengo " + PesoMex / Euro + "Euros")
 console.log("tengo " + PesoMex * Cuc + "cucs")
+
+
+
+var AñosCumplidos = 26
+var MesCumplidos = 2
+var DiasCumplidos = 0
+
+var TotaldeDias = AñosCumplidos*365 + MesCumplidos*30 + DiasCumplidos
+var HorasTotales = TotaldeDias * 24
+var MinTotales = HorasTotales * 60
+var SegundosTotales = MinTotales * 60
+console.log("segundos que has vivido" + segundos SegundosTotales)
+
