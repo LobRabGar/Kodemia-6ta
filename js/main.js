d4192c1db86f8d0363f20c756e556f3ab71364d1
lobsang <lob_slash@live.com>
2019-12-02 22:10:51 -0600

    ejercicio en clase

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("hello Koders")
+
+var PesoMex = 100
+var USDolar = 19.57
+var Euro = 21.67
+var Cuc = 26.5 / 0.0511285
+
+console.log( "tengo " + PesoMex / USDolar + "USD")
+console.log("tengo " + PesoMex / Euro + "Euros")
+console.log("tengo " + PesoMex * Cuc + "cucs")
